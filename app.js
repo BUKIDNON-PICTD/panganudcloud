79ce1bfa0b25be80260055fd44ef0b820056430c
Rufino John Aguilar <aguilarufino@gmail.com>
2019-04-01 16:24:00 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+io.on('connection', function (socket) {
+  socket.on('serveronline',function(serverid){
+  	console.log('serverid = ' + serverid + ' is CONNECTED')
+  	//socket.join(serverid);
+  });
+  
+  socket.on('disconnect', function () {
+    console.log('the client has disconnected!');
+  });
+  
+  socket.on('Message', function(data, fn) {
+   // console.log('pass data to client');
+    io.emit('Message', data);
+  });
+
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(process.env.PORT || 9000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Running Panganud Server at ", addr.address + ":" + addr.port);
+});
+
