d5b97bbdfd57d1ed1c288f6288114b79edcc9a58
RUFINO JOHN AGUILAR <aguilarufino@gmail.com>
2019-04-22 11:48:32 +0800

    register online and offline server

--- app.js
+++ app.js
@@ -2,16 +2,34 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-
+var numServers = 0
 io.on('connection', function (socket) {
+  var addedServer = false;
   socket.on('checkinserveronline',function(serverid){
-  	console.log('serverid = ' + serverid + ' is CONNECTED')
-    io.emit('onlineservers', serverid);
+    if (addedServer) return;
+
+    socket.servername=serverid;
+    ++numServers;
+    addedServer = true;
+    console.log(socket.servername + ' is ONLINE');
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    socket.broadcast.emit('server online', {
+      servername: socket.servername,
+      numServers: numServers
+    });
   });
   
-  socket.on('disconnect', function (serverid) {
-    console.log('serverid = ' + serverid + ' is DISCONNECTED')
-    io.emit('serveroffline', serverid);
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+      console.log(socket.servername + ' is OFFLINE');
+      socket.broadcast.emit('server offline', {
+        servername: socket.servername,
+        numServers: numServers
+      });
+    }
   });
 
   socket.on('Message', function(data, fn) {
