fe75367d7590ce3328f9c325a902b6b443628a5b
RUFINO JOHN AGUILAR <aguilarufino@gmail.com>
2019-04-23 14:20:27 +0800

    corrected passport

--- src/middleware/passport.js
+++ src/middleware/passport.js
@@ -1,4 +1,5 @@
-var User        = require('../models/user');
+// var User        = require('../models/user');
+var User2 = require('../models/user2');
 var JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt  = require('passport-jwt').ExtractJwt;
 var config      = require('../config/config');
@@ -9,14 +10,27 @@ var opts = {
 }
  
 module.exports = new JwtStrategy(opts, function (jwt_payload, done) {
-    User.findById(jwt_payload.id, function (err, user) {
-        if (err) {
-            return done(err, false);
+    //mongo version
+    // User.findById(jwt_payload.id, function (err, user) {
+    //     if (err) {
+    //         return done(err, false);
+    //     }
+    //     if (user) {
+    //         return done(null, user);
+    //     } else {
+    //         return done(null, false);
+    //     }
+    // });
+    //mysql version
+    User2.findOne({
+        where:{
+            id:jwt_payload.id
         }
-        if (user) {
+    }).then(user => {
+        if (user){
             return done(null, user);
-        } else {
-            return done(null, false);
         }
+    }).catch(err => {
+        return done(err, false);
     });
 });
