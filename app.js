398af8b39c460a8234fd335754581f4335b0f133
Rufino John <aguilarufino@gmail.com>
2020-07-16 11:33:08 +0800

    added covid page and messagepy

--- app.js
+++ app.js
@@ -17,144 +17,176 @@ app.use("/spagenda", express.static(__dirname + "/mnt/spagenda/"));
 io.origins("*:*");
 var passport = require("passport");
 
-var numServers = 0;
-let connectedServers = {};
-let serverList = [];
-io.on("connection", function(socket) {
-  var addedServer = false;
+// var numServers = 0;
+// let connectedServers = {};
+// let serverList = [];
 
-  socket.on("message", data => {
-    socket.broadcast.emit("message", {
-      servername: socket.servername,
-      message: data
-    });
-  });
 
-  socket.on("serverrequest", function(params) {
-    if (params.reciever in connectedServers) {
-      console.log("PROCESSING REQUEST FROM :" + params.sender);
-      const recieverSocket = connectedServers[params.reciever].id;
-      socket.to(recieverSocket).emit("serverrequest", params);
-    }
-  });
+var numClients = 0;
+let connectedClients = {};
+let clientList = [];
 
-  socket.on("serverresponse", function(data) {
-    if (data.sender in connectedServers) {
-      console.log("SENDING RESPONSE TO :" + data.reciever);
-      const senderSocket = connectedServers[data.reciever].id;
-      socket.to(senderSocket).emit("serverresponse", data.result);
-    }
-  });
+io.on("connection", function (socket) {
+  // var addedServer = false;
+  var addedClient = false;
 
-  setInterval(function() {
-    io.emit("connectedservers", {
-      serverList: serverList,
-      numServers: numServers
+  // socket.on("message", data => {
+  //   socket.broadcast.emit("message", {
+  //     servername: socket.servername,
+  //     message: data
+  //   });
+  // });
+
+  // socket.on("serverrequest", function (params) {
+  //   if (params.reciever in connectedServers) {
+  //     console.log("PROCESSING REQUEST FROM :" + params.sender);
+  //     const recieverSocket = connectedServers[params.reciever].id;
+  //     socket.to(recieverSocket).emit("serverrequest", params);
+  //   }
+  // });
+
+  // socket.on("serverresponse", function (data) {
+  //   if (data.sender in connectedServers) {
+  //     console.log("SENDING RESPONSE TO :" + data.reciever);
+  //     const senderSocket = connectedServers[data.reciever].id;
+  //     socket.to(senderSocket).emit("serverresponse", data.result);
+  //   }
+  // });
+
+  setInterval(function () {
+    io.emit("clientlistupdate", {
+      clientList: clientList
     });
   }, 10000);
 
-  //socket.emit('serverrequest','test',function(data){
-  //   console.log(data);
-  //});
-  // socket.emit('serverrequestralph','test',function(data){
-  //   console.log(data);
-  // });
-  // socket.emit('serverrequestjade','test',function(data){
-  //   console.log(data);
-  // });
-  // socket.emit('serverrequestrufy','test',function(data){
-  //   console.log(data);
-  // });
-  // app.get('/', function(req, res) {
-  //   socket.emit('serverrequest', req.body, function (data) {
-  //     res.json(data).status(200);
+
+  // socket.on("checkinserveronline", function (serverid) {
+  //   if (addedServer) return;
+
+  //   socket.servername = serverid;
+  //   ++numServers;
+  //   addedServer = true;
+  //   connectedServers = addServer(connectedServers, socket);
+  //   serverList.push(serverid);
+  //   console.log(socket.servername + " is ONLINE");
+  //   socket.emit("login", {
+  //     numServers: numServers
+  //   });
+  //   // const recieverSocket = connectedServers[serverid].id
+  //   // console.log(recieverSocket);
+  //   // socket.emit('serverrequest'+recieverSocket,recieverSocket,function(data){
+  //   // 	 console.log(data);
+  //   // });
+  //   socket.broadcast.emit("serveronline", {
+  //     serverList: serverList,
+  //     servername: socket.servername,
+  //     numServers: numServers
   //   });
   // });
-  // socket.emit('serverrequest','test',function(data){
-  //   console.log(data);
+
+
+
+  // socket.on("disconnect", () => {
+  //   if (addedServer) {
+  //     --numServers;
+  //     console.log(socket.servername + " is OFFLINE");
+  //     connectedServers = removeServer(connectedServers, socket.servername);
+  //     serverList.splice(serverList.indexOf(socket.servername), 1);
+  //     socket.broadcast.emit("serveroffline", {
+  //       serverList: serverList,
+  //       servername: socket.servername,
+  //       numServers: numServers
+  //     });
+  //   }
   // });
-  //app.post('/serverrequest', passport.authenticate('jwt', { session: false }), (req, res) => {
-  //  console.log("REQUEST FROM: " + req.body.sender + " to " + req.body.reciever);
-  //  //socket.emit('serverrequest', req.body, function (data) {
-  //  //	res.json(data).status(200);
-  //  //});
-  //	socket.broadcast.emit('serverrequest', req.body);
-  //	io.on('serverresponse', resppHandler);
-  //	function resppHandler(data) {
-  //		console.log(data);
-  //		res.json(data).status(200);
-  //	}
-  //socket.on('serverresponse', resphandler()function(data){
-  //	//console.log(data.reciever + "-TRU");
-  //	if(data.reciever === lguid){
-  //		res.json(data.result).status(200);
-  //	}
-  //});
-  //ioreq(io).request("serverrequest",req.body)
-  //	.then(function(data){
-  //	//console.log(res); // get {height: 528, width: 924}
-  //	if(data.reciever === req.body.reciever){
-  //		res.json(data.result).status(200);
-  //	}
-  //});
-  //});
+  // socket.on("messagepy", data => {
+    
+  // });
 
-  socket.on("checkinserveronline", function(serverid) {
-    if (addedServer) return;
-
-    socket.servername = serverid;
-    ++numServers;
-    addedServer = true;
-    connectedServers = addServer(connectedServers, socket);
-    serverList.push(serverid);
-    console.log(socket.servername + " is ONLINE");
-    socket.emit("login", {
-      numServers: numServers
+  socket.on("message", data => {
+    io.emit("update", {
+      client: socket,
+      profile: data.profile,
+      timestamp: data.timestamp
     });
+  });
+ 
+  socket.on("clientcheckin", function (clientinfo) {
+    if (addedClient) return;
+    // console.log( clientinfo.find(i => i.name === 'clientid').value);
+    socket = clientinfo;
+    ++numClients;
+    addedClient = true;
+    connectedClients = addClient(connectedClients, socket);
+    clientList.push(socket);
+    console.log(socket.find(i => i.name === 'clientname').value + " is ONLINE");
+    // socket.emit("login", {
+    //   numClients: numClients
+    // });
     // const recieverSocket = connectedServers[serverid].id
     // console.log(recieverSocket);
     // socket.emit('serverrequest'+recieverSocket,recieverSocket,function(data){
     // 	 console.log(data);
     // });
-    socket.broadcast.emit("serveronline", {
-      serverList: serverList,
-      servername: socket.servername,
-      numServers: numServers
+    io.emit("clientlistupdate", {
+      clientList: clientList
     });
   });
 
   socket.on("disconnect", () => {
-    if (addedServer) {
-      --numServers;
-      console.log(socket.servername + " is OFFLINE");
-      connectedServers = removeServer(connectedServers, socket.servername);
-      serverList.splice(serverList.indexOf(socket.servername), 1);
-      socket.broadcast.emit("serveroffline", {
-        serverList: serverList,
-        servername: socket.servername,
-        numServers: numServers
+    if (addedClient) {
+      --numClients;
+      console.log(socket.find(i => i.name === 'clientname').value + " is OFFLINE");
+      connectedClients = removeClient(connectedClients, socket.find(i => i.name === 'clientid').value);
+      clientList.splice(clientList.indexOf(socket), 1);
+      io.emit("clientlistupdate", {
+        clientList: clientList
       });
     }
   });
 });
 
-function addServer(serverList, socket) {
-  let newList = Object.assign({}, serverList);
-  newList[socket.servername] = socket;
+
+// function addServer(serverList, socket) {
+//   let newList = Object.assign({}, serverList);
+//   newList[socket.servername] = socket;
+//   return newList;
+// }
+
+// function removeServer(serverList, servername) {
+//   let newList = Object.assign({}, serverList);
+//   delete newList[servername];
+//   return newList;
+// }
+
+function addClient(clientList, socket) {
+  let newList = Object.assign({}, clientList);
+  newList[socket.clientid] = socket;
   return newList;
 }
 
-function removeServer(serverList, servername) {
-  let newList = Object.assign({}, serverList);
-  delete newList[servername];
+function removeClient(clientList, clientid) {
+  let newList = Object.assign({}, clientList);
+  delete newList[clientid];
   return newList;
 }
+
+
+app.get("/covidsubay", function (req, res) {
+  res.render("covid/index");
+});
+
+app.post('/messagepy', (req, res) => {
+  console.log(req.body)
+  io.emit("messagepy", req.body.message);  
+  return res.json({ msg: `Message Sent!` });
+});
 
 function getMostRecentFileName(dir) {
   var files = fs.readdirSync(dir);
 
   // use underscore for max()
-  return _.max(files, function(f) {
+  return _.max(files, function (f) {
     var fullpath = path.join(dir, f);
 
     // ctime = creation time is used
@@ -164,16 +196,16 @@ function getMostRecentFileName(dir) {
 }
 
 // console.log(tree);
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   let agenda = req.query.agenda;
   let file = req.query.file;
 
   let root = dirTree("/usr/src/app/mnt/spagenda");
 
   var agendasort = [];
-  root.children.forEach(function(agenda) {
+  root.children.forEach(function (agenda) {
     var agendaitemsort = [];
-    agenda.children.forEach(function(agendaitem) {
+    agenda.children.forEach(function (agendaitem) {
       if (
         agendaitem.type == "file" &&
         (agendaitem.extension == ".htm" || agendaitem.extension == ".html")
@@ -183,21 +215,24 @@ app.get("/", function(req, res) {
         agendaitemsort.push(agendaitem);
       }
     });
-    if (agenda.type == 'directory'){
+    if (agenda.type == 'directory') {
       var stats = fs.statSync(agenda.path)
       agenda.ctime = stats.ctime.getTime()
       agendasort.push(agenda);
     }
-    agendaitemsort.sort(function(a,b) {
-        return b.mtime - a.mtime;
+    agendaitemsort.sort(function (a, b) {
+      return b.mtime - a.mtime;
     });
     agenda.children = agendaitemsort;
   });
-  agendasort.sort(function(a,b){
+  agendasort.sort(function (a, b) {
     return b.ctime - a.ctime;
   });
   root.children = agendasort;
-
+  if (!agenda) {
+    agenda = root.children[0].name
+    file = root.children[0].children[0].name
+  }
   res.render("pages/index", {
     agendalist: root.children,
     currentagenda: agenda,
@@ -208,7 +243,7 @@ app.get("/", function(req, res) {
 server.listen(
   process.env.PORT || 9000,
   process.env.IP || "0.0.0.0",
-  function() {
+  function () {
     var addr = server.address();
     console.log("Panganud Server at ", addr.address + ":" + addr.port);
   }
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,9 @@ var passport	    = require('passport');
 
 routes.post('/register', userController.registerUser);
 routes.post('/login', userController.loginUser);
- 
+
+
+
 routes.get('/special', passport.authenticate('jwt', { session: false }), (req, res) => {
     return res.json({ msg: `Hey ${req.user.email}! I open at the close.` });
 });
