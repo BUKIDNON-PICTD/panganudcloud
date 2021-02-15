025834a4a4ba9bd4be62090712bd973b8395855d
hoycid <quilangcidrex@gmail.com>
2021-02-15 10:48:14 +0800

    renamed variable

--- src/controller/vaccinesurveydashboardcontroller.js
+++ src/controller/vaccinesurveydashboardcontroller.js
@@ -3,8 +3,8 @@ var Item = require('../models/vaccinesurvey_totals');
 
 exports.getTotals = async (req, res) => {
     try {
-        const items = await Item.findOne();
-        return res.status(200).json(items);
+        const item = await Item.findOne();
+        return res.status(200).json(item);
     } catch (error) {
         return res.status(500).send(error.message);
     }
